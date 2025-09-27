e7db1627cb6c93a9ca91aa0c1c95b2118ec4e716
5ch1z0 <5ch1z0@users.noreply.github.com>
2025-09-27 19:38:45 +0200

    updated code

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+const links = {
+    "Github" = "https://github.com/5ch1z0",
+    "TryHackMe" = "https://tryhackme.com/p/5CH1Z0"
+}
+
+
 function main() {
     const htmlHeader = document.querySelector("body > div#header");
     const htmlMain = document.querySelector("body > div#main");
 
-    let list_item = document.createElement("div");
-    list_item.innerText = "list item";
-    htmlMain.appendChild(list_item);
+    const list = htmlMain.querySelector("");
+    for (const [key, value] of Obhect.entries(links)) {
+        const item = document.createElement("div");
+        const a = document.createElement("a");
+        a.innerText = key;
+        a.setAttribute('href', value);
+
+        item.appendChild(a);
+        list.appendChild(item);
+    }
 }
 
+
+// Load main if DOMContentLoaded
 document.addEventListener("DOMContentLoaded", (event) => {
     main();
 });
